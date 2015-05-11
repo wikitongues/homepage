4c46b12fd3f5c7736c7b941f6e5c2f659b86b60a
Freddie Andrade <faandrade@gmail.com>
2015-05-10 23:17:04 -0400

    removed reset scroll console log

--- js/src/application.js
+++ js/src/application.js
@@ -5,7 +5,6 @@ App.ResetScroll = Ember.Mixin.create({
   activate: function() {
     this._super();
     window.scrollTo(0,0);
-    console.log("resetting scrolls")
   }
 });
 App.ApplicationAdapter = DS.FixtureAdapter.extend({
