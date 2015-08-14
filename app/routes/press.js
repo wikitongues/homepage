cb9d99128e2974ce3cb982c59554cf162e380eba
Fredward <faandrade@gmail.com>
2015-08-14 17:18:13 -0400

    fix merge issues

--- app/routes/press.js
+++ app/routes/press.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 import ResetScroll from 'homepage-cli/mixins/reset-scroll';
 
 export default Ember.Route.extend(ResetScroll, {
-
-export default Ember.Route.extend({
   model: function() {
     return this.store.findAll("press");
   }
